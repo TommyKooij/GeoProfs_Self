d7b6922f9ea7cfabda6dac5238d9e744e559599f
TommyKooij <74959713+TommyKooij@users.noreply.github.com>
2023-04-05 10:40:55 +0200

    push 1

---
+++ branch4/GeoProfs2/GeoProfs/GeoProfs/Models/CalendarEvent.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeoProfs.Models
+{
+
+    public class CalendarEvent
+    {
+        [Key]
+
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Title cannot be longer than 50 characters.")]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "Description cannot be longer than 500 characters.")]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "End Date")]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Type cannot be longer than 500 characters.")]
+        [Display(Name = "Type")]
+        public string Type { get; set; }
+    }
+}
---
+++ branch4/GeoProfs2/GeoProfs/GeoProfs/Pages/Calendars/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GeoProfs.Data;
+using GeoProfs.Models;
+
+namespace GeoProfs.Pages.Calendars
+{
+    public class IndexModel : PageModel
+    {
+        private readonly GeoProfs.Data.GeoProfsContext _context;
+
+        public IndexModel(GeoProfs.Data.GeoProfsContext context)
+        {
+            _context = context;
+        }
+
+        public IList<CalendarEvent> CalendarEvent { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.CalendarEvents != null)
+            {
+                CalendarEvent = await _context.CalendarEvents.ToListAsync();
+            }
+        }
+    }
+}
